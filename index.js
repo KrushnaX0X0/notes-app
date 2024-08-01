0fcaf36bb6496f6ea70b8d2f389d6eee33392e3a
KrushnaX0X0 <krushnajagtap2004@gmail.com>
2024-08-01 19:49:07 +0530

    add js and list

---
+++ index.js
@@ -0,0 +1,12 @@
+let noties =["bay vagitabe","go to the gym","go to school","go to clg"]
+function lodesnoties(){
+    let notescontiner = document.getElementById("notes-continer");
+    
+
+    for ( let note of noties ){
+       console.log(note)
+    }
+}
+
+lodesnoties()
+
